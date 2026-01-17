73adad23cc7f8219c43926395e55e4d68a07f119
saurabhrksss <137519468+saurabhrksss@users.noreply.github.com>
2026-01-17 22:01:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (18.75%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -4,8 +4,8 @@ public:
         int len=s.length();
         for(int k=0;k<=len-1;k++){
             string temp=s;
-            reverse(temp.begin(),temp.begin()+k+1);
-            reverse(temp.begin()+k+1,temp.end());
+            reverse(temp.begin(),temp.begin()+k);
+            reverse(temp.begin()+k,temp.end());
             reverse(temp.begin(),temp.end());
             if(temp==goal) return 1;
         }
