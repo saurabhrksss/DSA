27d422e1aed67829d9349791382f3455d0957051
saurabhrksss <137519468+saurabhrksss@users.noreply.github.com>
2026-01-25 21:40:37 +0530

    Time: 143 ms (9.16%) | Memory: 54.9 MB (8.01%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
     vector<vector<int>>dp;
+    bool isallstar(int j,const string &p)
+    {
+        int m=p.size();
+        for(int i=j;i<m;i++)
+        if(p[i]!='*') return false;
+        return true;
+    }
     bool match(int i,int j,const string &s,const string &p)
     {
         int n=s.size();
         int m=p.size();
-        if(i==n && j==m ) return dp[i][j]=1;
+        if(i==n && j==m) return dp[i][j]=1;
         if(j==m) return dp[i][j]=0;
         if(i==n)
         {
-            if(p[j]=='*')
-            {
-                if(match(i,j+1,s,p)) return dp[i][j]=1;
-            }
-            return dp[i][j]=0;
+            return isallstar(j,p);
         }
         if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==p[j] && islower(s[i]) && islower(p[j])) return dp[i][j]=match(i+1,j+1,s,p);
-        if(s[i]!=p[j] && islower(s[i]) && islower(p[j])) return dp[i][j]=false;
-        if(p[j]=='?') return match(i+1,j+1,s,p);
+        //match
+        if(s[i]==p[j] || p[j]=='?')
+        return dp[i][j]=match(i+1,j+1,s,p);
+        //*
         if(p[j]=='*')
-        {
-            return dp[i][j]=match(i,j+1,s,p) | match(i+1,j,s,p);
-        }
-        return dp[i][j]=false;
+        return dp[i][j]=match(i,j+1,s,p)|match(i+1,j,s,p);
+        //not equal
+        return dp[i][j]=0;
     } 
     bool isMatch(string s, string p) {
         int n=s.size();
