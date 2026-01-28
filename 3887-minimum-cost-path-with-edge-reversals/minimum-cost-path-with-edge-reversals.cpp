ecd76c7d69b3bc8f6b73df329a6cbb53000bde8a
saurabhrksss <137519468+saurabhrksss@users.noreply.github.com>
2026-01-28 06:18:29 +0530

    Time: 415 ms (24.35%) | Memory: 334.5 MB (21.08%) - LeetSync

--- 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.cpp
+++ 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.cpp
@@ -1,45 +1,40 @@
 class Solution {
 public:
+    const long long INF=1e18;
     int minCost(int n, vector<vector<int>>& edges) {
-        vector<vector<pair<int,int>>> out(n), in(n);
-        for (auto &e : edges) {
-            out[e[0]].push_back({e[1], e[2]});
-            in[e[1]].push_back({e[0], e[2]});
+        vector<vector<pair<int,int>>>adj(n);
+        vector<vector<pair<int,int>>>indegree(n);
+        for(auto it:edges)
+        {
+            adj[it[0]].push_back({it[1],it[2]});
+            indegree[it[1]].push_back({it[0],it[2]});
         }
-
-        const long long INF = 1e18;
-        vector<vector<long long>> dist(n, vector<long long>(2, INF));
-
-        priority_queue<array<long long,3>, vector<array<long long,3>>, greater<>> pq;
-        dist[0][0] = 0;
-        pq.push({0, 0, 0}); // cost, node, used
-
-        while (!pq.empty()) {
-            auto [cost, u, used] = pq.top();
+        vector<long long>dis(n,INF);
+        priority_queue< tuple<long long,int> ,vector<tuple<long long,int>>,greater<tuple<long long,int>>>pq;
+        pq.push({0,0});
+        dis[0]=0;
+        while(!pq.empty())
+        {
+            auto [distance,curr]=pq.top();
             pq.pop();
-
-            if (cost > dist[u][used]) continue;
-
-            // Normal edges
-            for (auto [v, w] : out[u]) {
-                if (dist[v][0] > cost + w) {
-                    dist[v][0] = cost + w;
-                    pq.push({dist[v][0], v, 0});
+            if(distance>dis[curr]) continue;
+            for(auto [child,wt]:adj[curr])
+            {
+                if(distance+wt<dis[child])
+                {
+                    dis[child]=distance+wt;
+                    pq.push({dis[child],child});
                 }
             }
-
-            // Reversed edges (only if switch unused)
-            if (used == 0) {
-                for (auto [v, w] : in[u]) {
-                    if (dist[v][0] > cost + 2LL * w) {
-                        dist[v][0] = cost + 2LL * w;
-                        pq.push({dist[v][0], v, 0});
-                    }
+            for(auto [parent,wt]:indegree[curr])
+            {
+                if(dis[curr]+2ll*wt<dis[parent])
+                {
+                    dis[parent]=dis[curr]+2*wt;
+                    pq.push({dis[parent],parent});
                 }
             }
         }
-
-        long long ans = min(dist[n-1][0], dist[n-1][1]);
-        return ans == INF ? -1 : ans;
+        return dis[n-1]==INF ? -1 : dis[n-1];
     }
 };
