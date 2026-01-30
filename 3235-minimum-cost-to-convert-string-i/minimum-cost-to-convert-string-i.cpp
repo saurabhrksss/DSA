191faa7bca597ec93f3a5ccef706808fa3f40790
saurabhrksss <137519468+saurabhrksss@users.noreply.github.com>
2026-01-31 01:20:43 +0530

    Time: 26 ms (70.87%) | Memory: 109.1 MB (21.75%) - LeetSync

--- 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
@@ -11,14 +11,8 @@ public:
             adj[u].push_back({v,wt});
             dis[u][v]=std::min(dis[u][v],wt);
         } 
-        for(int i=0;i<26;i++)
-        {
-            for(int j=0;j<26;j++)
-            {
-                if(i==j)
-                dis[i][j]=0;
-            }
-        }
+        for(int i=0;i<26;i++) dis[i][i]=0;
+        
         for(int k=0;k<26;k++)
         {
             for(int i=0;i<26;i++)
