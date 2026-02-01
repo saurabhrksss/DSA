73ad3e7037ee64f17d97c6733991a0b4d32060bf
saurabhrksss <137519468+saurabhrksss@users.noreply.github.com>
2026-02-01 07:55:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Subset With Target Sum II/count-subset-with-target-sum-ii.cpp
+++ Difficulty: Medium/Count Subset With Target Sum II/count-subset-with-target-sum-ii.cpp
@@ -1,40 +1,39 @@
-#define ll long long int
 class Solution {
   public:
+    vector<int>left;
+    vector<int>right;
+    unordered_map<int,int>mp;
+    int ans=0;
+    void recleft(int index,int sum)
+    {
+        int n=left.size();
+        if(index==n)
+        {
+            mp[sum]++;
+            return;
+        }
+        recleft(index+1,sum);
+        recleft(index+1,sum+left[index]);
+    }
+    void recright(int index,int sum,int k)
+    {
+        int n=right.size();
+        if(index==n)
+        {
+            ans=ans+mp[k-sum];
+            return;
+        }
+        recright(index+1,sum,k);
+        recright(index+1,sum+right[index],k);
+    }
     int countSubset(vector<int> &arr, int k) {
         // code here
         int n=arr.size();
         int mid=n/2;
-        vector<int> left(arr.begin(),arr.begin()+mid);
-        vector<int> right(arr.begin()+mid,arr.end());
-        map<ll,ll>mp;
-        int m=right.size();
-        for(int mask=0;mask<(1LL<<m);mask++)
-        {
-            ll sum=0;
-            for(int i=0;i<m;i++)
-            {
-                if(mask&(1<<i))
-                {
-                    sum+=right[i];
-                }
-            }
-            mp[sum]++;
-        }
-        int ans=0;
-        m=left.size();
-        for(int mask=0;mask<(1LL<<m);mask++)
-        {
-            ll sum=0;
-            for(int i=0;i<m;i++)
-            {
-                if(mask&(1LL<<i))
-                {
-                    sum+=left[i];
-                }
-            }
-            ans+=mp[k-sum];
-        }
+        left.assign(arr.begin(),arr.begin()+mid);
+        right.assign(arr.begin()+mid,arr.end());
+        recleft(0,0);
+        recright(0,0,k);
         return ans;
     }
 };
